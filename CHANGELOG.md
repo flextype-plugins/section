c54f19ece918b34ac3fba92f3eaa420159fdb16e
Awilum <awilum@msn.com>
2020-08-25 17:20:51 +0300

    Section 1.8.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+<a name="1.8.0"></a>
+# [1.8.0](https://github.com/flextype-plugins/section) (2020-08-25)
+
+### Features
+
+* **core** update code base for new Flextype 0.9.11
+
 <a name="1.7.0"></a>
 # [1.7.0](https://github.com/flextype-plugins/section) (2020-08-19)
 
--- shortcodes/SectionShortcode.php
+++ shortcodes/SectionShortcode.php
@@ -6,11 +6,11 @@ use Thunder\Shortcode\ShortcodeFacade;
 use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 
 // Shortcode: [section_create id=section-uid]Section content here[/section_create]
-$flextype->container('shortcode')->addHandler('section_create', function (ShortcodeInterface $s) use ($flextype) {
-    $flextype->container('registry')->set('sections.' . $s->getParameter('id'), $s->getContent());
+flextype('shortcode')->addHandler('section_create', function (ShortcodeInterface $s) {
+    flextype('registry')->set('sections.' . $s->getParameter('id'), $s->getContent());
 });
 
 // Shortcode: [section_fetch id=section-uid]
-$flextype->container('shortcode')->addHandler('section_fetch', function (ShortcodeInterface $s) use ($flextype) {
-    return $flextype->container('registry')->get('sections.' . $s->getParameter('id'));
+flextype('shortcode')->addHandler('section_fetch', function (ShortcodeInterface $s) {
+    return flextype('registry')->get('sections.' . $s->getParameter('id'));
 });
